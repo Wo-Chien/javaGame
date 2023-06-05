240f96fd311ab8370ef29ee4b2dc730dc06a15c2
jelinjon <jelinjon@fel.cvut.cz>
2023-06-05 16:33:00 +0200

    5.6. 2023 mvc start

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,10 +1,12 @@
 package game;
 
+import game.View.GameMenu;
+
+/**
+ * create a new instance of game menu that lets you choose between lvls and adjust setting and so on
+ */
 public class Main {
     public static void main(String[] args) {
-        // todo game menu
-        /**
-         * game menu that lets you choose between lvls and adjust setting and so on
-         */
+        new GameMenu();
     }
 }
